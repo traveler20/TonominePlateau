2f057c8987dceea7f70e9737a776126c3c54ca63
大坪　旅人 <60683849+tabito-otsubo@users.noreply.github.com>
2020-12-09 12:52:11 +0900

    Add files via upload

---
+++ js/detail.js
@@ -0,0 +1,26 @@
+
+  //ハンバーガーメニューの開閉
+  $('.burger-btn').on('click',function(){
+    $('.header-nav').fadeToggle(300);
+    $('.burger-btn').toggleClass('cross');
+    $('body').toggleClass('noscroll');
+  });
+
+
+  /*スライド画像部分の定義*/
+  const slider = new Swiper ('#slider', {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    autoplay:3000
+  });
+  /*スライド画像サムネイル部分の定義*/
+  const thumbs = new Swiper('#thumbs', {
+    centeredSlides: true,
+    spaceBetween: 10,
+    slidesPerView: "auto",
+    touchRatio: 0.2,
+    slideToClickedSlide: true
+  });
+  /*それぞれの動作に合わせて動く*/
+  slider.params.control = thumbs;
+  thumbs.params.control = slider;
--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
-
 //sticky header
-
   $(function() {
     var $win = $(window),
         $fv = $('.fv'),
         $header = $('.header')
         fvHeight = $fv.outerHeight(),
         fixedClass = 'fixed';
-  
+
   $win.on('load scroll',function(){
     var value = $(this).scrollTop();
       if($win.width()>768)
-  
+
         if ( value > fvHeight) {
           $header.addClass(fixedClass);
-          
         } else {
           $header.removeClass(fixedClass);
-          
         }
-      
-  
+
     });
   });
-
 
   //ファーストビューのslider
   $('.slider').slick({
@@ -34,10 +28,29 @@
     cssEase: 'linear'
   });
 
-  
+
   //ハンバーガーメニューの開閉
   $('.burger-btn').on('click',function(){
     $('.header-nav').fadeToggle(300);
     $('.burger-btn').toggleClass('cross');
     $('body').toggleClass('noscroll');
   });
+
+
+  /*スライド画像部分の定義*/
+  const slider = new Swiper ('#slider', {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    autoplay:3000
+  });
+  /*スライド画像サムネイル部分の定義*/
+  const thumbs = new Swiper('#thumbs', {
+    centeredSlides: true,
+    spaceBetween: 10,
+    slidesPerView: "auto",
+    touchRatio: 0.2,
+    slideToClickedSlide: true
+  });
+  /*それぞれの動作に合わせて動く*/
+  slider.params.control = thumbs;
+  thumbs.params.control = slider;
